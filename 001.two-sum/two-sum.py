3dd52345f95cfaf9ee1c68d57dbc1ff0b947f238
x <shuati24@gmail.com>
2019-06-19 00:12:51 -0700

    add test case

--- 001.two-sum/two-sum.py
+++ 001.two-sum/two-sum.py
@@ -11,3 +11,8 @@ class Solution(object):
         return [d[target - num], i]
       d[num] = i
     # no special case handling because it's assumed that it has only one solution
+
+if __name__ == '__main__':
+    # begin
+    s = Solution()
+    print(s.twoSum([3, 2, 4], 6))
